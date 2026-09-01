aaf81475245c99946369922da771f8a757760f82
AaronMax1 <aaron1234156@outlook.com>
2026-09-01 10:46:31 +0800

    Release Robinhood mainnet frontend

--- contract-config.js
+++ contract-config.js
@@ -1,11 +1,11 @@
 window.MINING_CONFIG = {
-  "chainId": 46630,
-  "address": "0x9326F58C93cF7A4F38f947bb03253c8281366b96",
-  "multicallAddress": "0xb97D33637f803BBacB1B5D646FbED3F2B29be7D9",
-  "tokenAddress": "0x249BD014C10Cb33c5beb1293Bb0b0A966b09Bc67",
-  "eligibilityAddress": "0xFDf4f97c320e771466a0929579dbC749D1C374A4",
+  "chainId": 4663,
+  "address": "0x2f0e6DF8F374865A9dD50ABE3D0D5DfAD270258b",
+  "multicallAddress": "0x1bF06d609da0ed2E374BD759480C57499Dcbe93e",
+  "tokenAddress": "0xD3ff8f5AFF2D9F8a98081305Ba89482bea50D727",
+  "eligibilityAddress": "0xCad35B1d45435847F4a1C047B85eBf001C7DCac7",
   "planetEligibilityAddresses": [
-    "0xFDf4f97c320e771466a0929579dbC749D1C374A4",
+    "0xCad35B1d45435847F4a1C047B85eBf001C7DCac7",
     "0x0000000000000000000000000000000000000000",
     "0x0000000000000000000000000000000000000000",
     "0x0000000000000000000000000000000000000000"
@@ -14,13 +14,13 @@ window.MINING_CONFIG = {
   "tokenRuntimeCodeHash": "0x53c55d968a305a90ee4f8066f34a5084c1f1983fe82ae0559e6a985378be5a4f",
   "multicallRuntimeCodeHash": "0x7458a64db38fae12e4c68a91190137bdaa06f7237175d31c41164461972bdf70",
   "eligibilityRuntimeCodeHash": "0xbee0fe4f0cd25cea1c0b4e36955704e1ca8367d3d3fa55a40d5fae3c8a801c15",
-  "networkName": "Robinhood Chain Testnet",
-  "readRpcUrl": "https://rpc.testnet.chain.robinhood.com",
+  "networkName": "Robinhood Chain",
+  "readRpcUrl": "https://rpc.mainnet.chain.robinhood.com",
   "walletRpcUrls": [
-    "https://rpc.testnet.chain.robinhood.com"
+    "https://rpc.mainnet.chain.robinhood.com"
   ],
   "blockExplorerUrls": [
-    "https://explorer.testnet.chain.robinhood.com"
+    "https://robinhoodchain.blockscout.com"
   ],
   "tokenName": "S3",
   "tokenSymbol": "S3",
